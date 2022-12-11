0a78e15e86b0c6eb7885ab66b69505d7cf6a4171
vaibhav jadhav <95289821+va1bhav22@users.noreply.github.com>
2022-12-12 00:35:11 +0530

    requireauth

--- FireBaseCrudApp/app/src/App.js
+++ FireBaseCrudApp/app/src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import {Routes,Route} from "react-router-dom"
+import {Routes,Route, Navigate} from "react-router-dom"
 import Home from "./Pages/Home/Home";
+import List from "./Pages/List/List";
 import Login from "./Pages/Login/Login";
 import Single from "./Pages/Single/Single";
 function App() {
+
+  const currentUser = false ;
+////////////////////////////// Required Authenticatiion /////////////////////////////////////////////
+  const RequireAuth=({children})=>{
+    return currentUser ? (children) : <Navigate to='/login' />
+  }
+  ////////////////////////////// Required Authenticatiion End /////////////////////////////////////////////
   return <>
     <Routes>
-        <Route path="/" element={<Home/>}></Route>
-        <Route path="/login" element={<Login/>}></Route>
-        <Route path="/single" element={<Single/>}></Route>
+       <Route path="/login" element={<Login/>}></Route>
+        <Route path="/" element={<RequireAuth><Home/></RequireAuth>}></Route>
+        <Route path="/single" element={<RequireAuth><Single/></RequireAuth>}></Route>
+        <Route path="" element={<RequireAuth><Single/></RequireAuth>}></Route>
+        <Route path="" element={<List/>}></Route>
+
         {/* <Route path="" element={}></Route>
-        <Route path="" element={}></Route>
-        <Route path="" element={}></Route>
         <Route path="" element={}></Route>
         <Route path="" element={}></Route> */}
 
--- FireBaseCrudApp/app/src/Pages/Login/Login.jsx
+++ FireBaseCrudApp/app/src/Pages/Login/Login.jsx
@@ -17,6 +17,7 @@ const Login = () => {
         // Signed in
         const user = userCredential.user;
         // console.log(user);
+        navigate("/")
 
       })
       .catch((error) => {
