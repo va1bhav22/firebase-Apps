3d7c7a2d4051234e7e92549a2d765874a48ba242
vaibhav jadhav <95289821+va1bhav22@users.noreply.github.com>
2022-12-14 01:07:50 +0530

    navcom

--- FireBaseCrudApp/app/src/Components/navbar/Navbar.jsx
+++ FireBaseCrudApp/app/src/Components/navbar/Navbar.jsx
@@ -28,9 +28,12 @@ const Navbar = () => {
           </div>
           <div className="item">
             <NotificationsNoneOutlinedIcon className="icon" />
+            <div className="counter">1</div>
+
           </div>
           <div className="item">
             <ChatBubbleOutlineOutlinedIcon className="icon" />
+            <div className="counter">2</div>
           </div>
           <div className="item">
             <ListOutlinedIcon className="icon" />
