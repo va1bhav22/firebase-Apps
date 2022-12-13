237b77c3a0d342267c3ef984b09b7a5e17c6957e
vaibhav jadhav <95289821+va1bhav22@users.noreply.github.com>
2022-12-13 20:56:24 +0530

    homesidebar

--- FireBaseCrudApp/app/src/Pages/Home/Home.jsx
+++ FireBaseCrudApp/app/src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import Sidebar from '../../Components/sidebar/Sidebar'
-
+import "./home.scss"
 const Home = () => {
   return (
     <div className='home'>
       <Sidebar/>
+      <div className="homeContainer">
+         container
+      </div>
     </div>
   )
 }
