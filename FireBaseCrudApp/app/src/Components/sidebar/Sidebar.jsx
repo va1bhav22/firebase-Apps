69335933b3de33e9867f65dd036ca5081063c59f
vaibhav jadhav <95289821+va1bhav22@users.noreply.github.com>
2022-12-13 22:19:20 +0530

    saas

--- FireBaseCrudApp/app/src/Components/sidebar/Sidebar.jsx
+++ FireBaseCrudApp/app/src/Components/sidebar/Sidebar.jsx
@@ -1,5 +1,17 @@
 import React from "react";
 import "./sidebar.scss";
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import Person2OutlinedIcon from '@mui/icons-material/Person2Outlined';
+import LocalShippingIcon from "@mui/icons-material/LocalShipping";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import StoreIcon from "@mui/icons-material/Store";
+import InsertChartIcon from "@mui/icons-material/InsertChart";
+import SettingsApplicationsIcon from "@mui/icons-material/SettingsApplications";
+import ExitToAppIcon from "@mui/icons-material/ExitToApp";
+import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
+import SettingsSystemDaydreamOutlinedIcon from "@mui/icons-material/SettingsSystemDaydreamOutlined";
+import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 const Sidebar = () => {
   return (
     <div className="sidebar">
@@ -9,17 +21,58 @@ const Sidebar = () => {
       <hr />
       <div className="center">
         <ul>
+          <p className="title">MAIN</p>
           <li>
+            <DashboardIcon className="icon"/>
             <span>Dashboard</span>
           </li>
+          <p className="title">LISTS</p>
           <li>
-            <span>Dashboard</span>
+            <Person2OutlinedIcon className="icon"/>
+            <span>Users</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <LocalShippingIcon className="icon" />
+            <span>Products</span>
           </li>
           <li>
-            <span>Dashboard</span>
+            <CreditCardIcon className="icon"/>
+            <span>Orders</span>
+          </li>
+          <li>
+            <StoreIcon className="icon"/>
+            <span>Delivery</span>
+          </li>
+          <p className="title">USEFUL</p>
+          <li>
+            <InsertChartIcon className="icon"/>
+            <span>Stats</span>
+          </li>
+          <li>
+            <SettingsApplicationsIcon className="icon"/>
+            <span>Notifications</span>
+          </li>
+          <p className="title">SERVICE</p>
+          <li>
+            <ExitToAppIcon className="icon" />
+            <span>System Helth</span>
+          </li>
+          <li>
+            <NotificationsNoneIcon className="icon"/>
+            <span>Logs</span>
+          </li>
+          <li>
+            <SettingsSystemDaydreamOutlinedIcon className="icon"/>
+            <span>Settings</span>
+          </li>
+          <p className="title">USER</p>
+          <li>
+            <PsychologyOutlinedIcon className="icon"/>
+            <span>Profile</span>
+          </li>
+          <li>
+            <AccountCircleOutlinedIcon className="icon"/>
+            <span>Logout</span>
           </li>
         </ul>
       </div>
